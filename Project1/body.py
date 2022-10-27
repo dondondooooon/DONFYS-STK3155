e74602db67b70da1c1dedfa415ffffa201b3b3f1
dondondooooon <89593997+dondondooooon@users.noreply.github.com>
2022-10-27 20:42:05 +0200

    ferdig

--- Project1/body.py
+++ Project1/body.py
@@ -1,76 +1,218 @@
 from header import *     # Import header file
-from ridge import *
 from legs import *       # Import 
-
-# commandline_check()
 
 np.random.seed(2018) # Setting Random Seed constant
 
 # Read Commandline Arguments 
-parser = argparse.ArgumentParser(description="Choose which function for data")
+parser = argparse.ArgumentParser(description="Choose parameters for the program")
 parser.add_argument('-df','--data_func', metavar='', required=True, help='Data Function')
 parser.add_argument('-n','--max_degree', metavar='', required=True, help='Max Polynomial Degree')
 parser.add_argument('-N','--data_points', metavar='', required=True, help='Number of Datapoints')
-parser.add_argument('-eps','--noise_data', metavar='', required=True, help='Add Epsilon Noise?')
-parser.add_argument('-p','--print_results', metavar='', required=True, help='Print Results?')
-parser.add_argument('-skl','--skl_compare', metavar='', required=True, help='Compare to SciKitLearn')
+parser.add_argument('-q','--quiet_data',help='Specify to remove noise',action='store_true')
+parser.add_argument('-scl','--scale',metavar='', required=True, help='Scaling?')
+parser.add_argument('-reg','--reg_method', metavar='', required=True, help='Regression Method')
+parser.add_argument('-nb','--bootsiter', metavar='', required=True, help='Bootstrap Iteration')
+parser.add_argument('-nk','--kfold', metavar='', required=True, help='No. of k-Folds')
+parser.add_argument('-pr','--print_plot',help='Specify to print',action='store_true')
+parser.add_argument('-optlam','--opt_lambda',help='Specify to execute lambda array',action='store_true')
 args = parser.parse_args()
+
+# Dictionaries
+DataFunc = {"simple": simple_function,"frank": FrankeFunction, 'real': RealFunction} # Output Data Function to be used
+RegMethod = {"ols": OLSlinreg, 'ridge': Ridgelinreg, 'lasso': Lassolinreg} # Regression Method to be used
+Scaling = {"noscale": noscale, "yescale": scale_data} # To scale or not scale data 
+
 # Initialize Values & Containers
-### can prolly turn arguments to bool and save 4 lines of code
 n = int(args.max_degree) # Max Polynomial Degree
 N = int(args.data_points) # Number of datapoints
-noisy = args.noise_data.lower() == "noisy"
-printed = args.print_results.lower() == "print"
-sklcompare = args.skl_compare.lower() == "compare"
-title = f"n:{n}; N:{N}; noise:{noisy}"
-phi = np.arange(1,n+1)
-noise = np.random.uniform(0,1,N) # Random Noise
-x = np.sort(np.random.uniform(0,1,N)) 
-y = np.sort(np.random.uniform(0,1,N))
-# Data_Function
-if args.data_func.lower() == "simple": # Easy 1D Function
-     func = simple_function(x,noise,noisy) 
-     y = 0
-elif args.data_func.lower() == "frank": # Frank Function
-     func = FrankeFunction(x,y,noise,noisy)
+phi = np.arange(1,n+1) # Array of Polynomial Degree
+if args.quiet_data == False:
+     noise = np.random.uniform(0,1,N) * 0.2 # Random Noise
 else:
-     print('Command Line Error: Check your command line arguments\n',\
-          '\n Especially for -df DATA_FUNC')
-     exit(1)
-N_bs = 100 # make argpars later
-nlambdas = 5 #10 # 100
-lambdas = np.logspace(-4,4,nlambdas)
+     noise = 0
 
-# bmse, bbias, bvar = OLS_boots(x,y,n,func,phi,N_bs)
+# Initialize x,y, and Data Function
+x,y,func = DataFunc[args.data_func](N,noise,args.print_plot)
 
-# # Main magic happenings 
-msetrain, msetest, mskltrain, mskltest, r2train, r2test,\
-     rskltrain, rskltest = OLS_learning(x,y,n,func,phi,noisy) # OLS MSE [from header]
-bmse, bbias, bvar = OLS_boots(x,y,n,func,phi,N_bs)
-# ridge_msetrain, ridge_msetest, ridgeskltrain,\
-#      ridgeskltest = Ridge_learning(x,y,n,func,phi,nlambdas,lambdas)
-# rmsesamp = Ridge_Boots(x,y,n,func,phi,nlambdas,lambdas,N_bs)
-# plt.imshow(np.mean(rmsesamp,axis=0,keepdims=True).reshape(n,nlambdas))
-# plt.colorbar()
-# plt.show()
-# msetest = np.mean(rmsesamp,axis=0,keepdims=True).reshape(n,nlambdas)
-# min_ind = np.argmin(msetest)
-# mrow, mcol = np.where(msetest == msetest.ravel()[min_ind])
-# print("The lowest MSE is found at: Row:",mrow, "and Col:", mcol)
-# print(phi[mrow])
-# print(lambdas[mcol])
+# Resampling params
+N_boots = int(args.bootsiter)
+N_k = int(args.kfold)
+# Lambda params
+if args.opt_lambda == True:
+     if args.data_func == 'real':
+          Nlams = 20 # for terrain
+          lambdas = np.logspace(-10,1,Nlams)
+     else:
+          Nlams = 50 # for franke
+          lambdas = np.logspace(-18,1,Nlams)
+else:
+     lambdas = np.array([0.0001,0.001,0.01,0.1,100])
+title = f"n:{n}; N:{N}"
 
+'''
+The following lines of codes are the main regression code
+with each of their respective resampling method 
+'''
 
+def CrossValidResampling(x,y,n,func,phi,lmb,nK):
+     msesamp = np.zeros((n,nK))
+     tr_msesamp = np.zeros((n,nK))
+     sklmse = np.zeros(n)
+     ridge = Ridge(alpha = lmb)
 
-# # Plot Functions
-# simple1D(x,func)  # Plot 1D simple function
-# frankee(x,y,noise,noisy) # Plot 2D Franke Function
-# ols_first(msetrain,msetest,mskltrain,mskltest,r2train,r2test,\
-#      rskltrain,rskltest,phi,printed,sklcompare,title) # Plots + Prints OLS no sampling
-# beta_plot(noisy) # Plot beta evolution in complexity
-bOLSplot(phi,bmse,bbias,bvar,msetest,N_bs)
-# RidgePlot(ridge_msetrain,ridge_msetest,ridgeskltrain,\
-#      ridgeskltest,phi,lambdas,title)
+     for degree in phi:
+          X = create_X(x,y,degree) # Build Design Matrix
+          X_test_notused, y_test_notused = np.copy(X), np.copy(func)  # Dummy containers
+          X, X_test_notused, func, y_test_notused =\
+               Scaling[args.scale](X,X_test_notused,func,y_test_notused) # Scale the Data
 
-# plt.plot(np.log(np.mean(bmse,axis=1,keepdims=True)))
-# plt.show()
+          _kfold = KFold(n_splits = nK) # Create folds
+          j = 0
+          for itr,jte in _kfold.split(X):
+               Xtr, ytr = X[itr], func[itr]
+               Xte, yte = X[jte], func[jte]
+               
+               beta = RegMethod[args.reg_method](Xtr,ytr,lmb)
+
+               ytilde = Xtr @ beta
+               ypred = Xte @ beta
+               tr_msesamp[degree-1,j] = MSE_func(ytr,ytilde)
+               msesamp[degree-1,j] = MSE_func(yte,ypred)
+
+               j += 1
+          # SciKitLearn
+          estimated_mse_folds = cross_val_score(ridge, X, func,\
+               scoring='neg_mean_squared_error', cv=_kfold)
+          sklmse[degree-1] = np.mean(-estimated_mse_folds)
+     # Take mean of all mse in k-fold iterations
+     msetrain = np.squeeze( np.mean(tr_msesamp, axis=1, keepdims=True) )
+     msetest = np.squeeze( np.mean(msesamp, axis=1, keepdims=True) )
+     return msetest, msetrain, sklmse
+
+def BootstrapResampling(x,y,n,func,phi,lmb,nB):
+     var = np.zeros(n)
+     bias = np.zeros(n)
+     msesamp = np.zeros((n,nB))
+
+     for degree in phi:
+          X = create_X(x,y,degree) # Build Design Matrix
+
+          X_train, X_test, y_train, y_test = train_test_split\
+               (X,func, test_size = 0.2, random_state=1) # Splitting the Data
+          X_train, X_test, y_train, y_test =\
+               Scaling[args.scale](X_train,X_test,y_train,y_test) # Scale the Data
+
+          ypred = np.empty((y_test.shape[0],nB)) # Initialize predict model array
+
+          for boots in range(0,nB): # Bootstrap Iterations
+               X_trboot, y_trboot = bootstrap(X_train,y_train) # Sample Data
+
+               beta = RegMethod[args.reg_method](X_trboot,y_trboot,lmb) # Calculate Beta
+
+               ypred[:,boots] = ypr = (X_test @ beta).ravel()  # Testing
+               msesamp[degree-1,boots] = MSE_func(y_test,ypr) # Calculate MSE
+          
+          # Calculate Bias & Variance
+          bias[degree-1] = np.nanmean( (y_test - np.nanmean(ypred, axis=1, keepdims=True))**2 )
+          var[degree-1] = np.mean( np.nanvar(ypred, axis=1) )
+     return msesamp,bias,var
+
+def NoResampling(x,y,n,func,phi,lmb):
+    # Initiate Containers
+    degree_beta = []
+    r2test = np.zeros(n)
+    r2train = np.zeros(n)
+    MSE_test = np.zeros(n)
+    MSE_train = np.zeros(n)
+    R2_sklTest = np.zeros(n)
+    R2_sklTrain = np.zeros(n)
+    MSE_sklTest = np.zeros(n)
+    MSE_sklTrain = np.zeros(n)
+
+    for degree in phi:
+        X = create_X(x,y,degree) # Create Design Matrix
+        # Split the Data
+        X_train, X_test, y_train, y_test = train_test_split\
+            (X,func, test_size = 0.2, random_state=1) 
+        # Scale the Data
+        X_train, X_test, y_train, y_test = Scaling[args.scale](X_train,X_test,y_train,y_test)
+
+        beta = RegMethod[args.reg_method](X_train,y_train,lmb)
+        # plot_beta_save(degree_beta,beta) # For plotting beta as increasing order of polynomials 
+        
+        ytilde = X_train @ beta # Training
+        ypredict = X_test @ beta # Testing
+
+        # MSE & R2 score via own Algorithm
+        MSE_train[degree-1] = MSE_func(y_train,ytilde)  
+        MSE_test[degree-1] =  MSE_func(y_test,ypredict) 
+        r2train[degree-1] = R2(y_train,ytilde)
+        r2test[degree-1] = R2(y_test,ypredict)
+
+        # SciKitLearnRegCheck [Only OLS check though]
+        clf = skl.LinearRegression().fit(X_train,y_train) # fit_intercept=False ?
+        MSE_sklTrain[degree-1] = mean_squared_error(clf.predict(X_train),y_train)
+        MSE_sklTest[degree-1] = mean_squared_error(clf.predict(X_test),y_test)
+        R2_sklTrain[degree-1] = clf.score(X_train,y_train)
+        R2_sklTest[degree-1] = clf.score(X_test,y_test)
+        # For plotting beta as increasing order of polynomials    
+        # BetaValues = pd.DataFrame(degree_beta)
+        # BetaValues.to_csv(f'results/BetaValsDegNoise:{noisy}.csv', index=False)
+    return (MSE_train,MSE_test,MSE_sklTrain,MSE_sklTest,\
+        r2train,r2test,R2_sklTrain,R2_sklTrain)
+
+# Resampling Dictionary 
+Resampling = {"noresamp": NoResampling, "bootstrap": BootstrapResampling,\
+               "crossvalid": CrossValidResampling} # Resampling Method to be used
+
+# Function for calculating lambda dependency
+def lambda_mse(lambdas):
+     # Initialize MSE containers
+     msetrain, msetest = np.zeros((n,Nlams)), np.zeros((n,Nlams))
+     mskltrain, mskltest, r2train, r2test, rskltrain, rskltest\
+          = np.zeros(n), np.zeros(n), np.zeros(n), np.zeros(n), np.zeros(n), np.zeros(n) 
+     bbias, bbvar = np.zeros((Nlams,n)), np.zeros((Nlams,n))
+     bmse = np.zeros((Nlams,n,N_boots))
+     cvtest = np.copy(msetest) # np.zeros((Nlams,n,N_k))
+     cvtrain = np.copy(msetest) 
+     sklmse = np.copy(msetest)
+     i = 0
+     for lmb in lambdas:
+          msetrain[:,i], msetest[:,i], _mskltrain, _mskltest, _r2train, _r2test,\
+               _rskltrain, _rskltest = Resampling['noresamp'](x,y,n,func,phi,lmb)
+          bmse[i,:,:], bbias[i,:], bbvar[i,:] = Resampling['bootstrap'](x,y,n,func,phi,lmb,N_boots)
+          cvtest[:,i], cvtrain[:,i], sklmse[:,i] = Resampling['crossvalid'](x,y,n,func,phi,lmb,N_k)
+          i += 1
+     _bmse = np.squeeze(np.mean(bmse,axis=2,keepdims=True))
+     return msetrain,msetest,_bmse,bbias,bbvar,cvtest,cvtrain,sklmse
+
+# For OLS (i.e. regularization param is 0)
+if args.reg_method.lower() == 'ols':
+     lmb = 0
+     msetrain, msetest, mskltrain, mskltest, r2train, r2test,\
+          rskltrain, rskltest = Resampling['noresamp'](x,y,n,func,phi,lmb)
+     bmse, bbias, bbvar = Resampling['bootstrap'](x,y,n,func,phi,lmb,N_boots)
+     cvtest,cvtrain,sklcv = Resampling['crossvalid'](x,y,n,func,phi,lmb,N_k)
+else: # Call lambda dependency
+     msetrain, msetest, bmse, bbias, bbvar, cvtest, cvtrain, sklcv =\
+          lambda_mse(lambdas)
+
+# Plotting:
+# Initial mse plots 
+# ols_first(msetrain,msetest,mskltrain,mskltest,r2train,r2test,rskltrain,\
+#    rskltest,phi,False,False,title+" noise:True")
+# beta plot
+#beta_plot(False)
+# Figure7(phi,N_boots,msetest,bmse,bbias,bbvar,title)
+# Figure8(phi,N_k,cvtest,sklcv,title) # Printout my cv vs scitkit learn cv
+# Figure9(phi,bmse,cvtest)
+# FinalPlot(phi,cvtest,args.data_func,Nlams) # run only on -reg ols 
+
+"""
+# For Finding optimal lambda in ridge and lasso
+np.savetxt(f'{args.data_func}/{args.reg_method}/msetrain_Nlmb{Nlams}.txt', msetrain)
+np.savetxt(f'{args.data_func}/{args.reg_method}/msetest_Nlmb{Nlams}.txt', msetest)
+np.savetxt(f'{args.data_func}/{args.reg_method}/bmse_Nlmb{Nlams}.txt', bmse)
+np.savetxt(f'{args.data_func}/{args.reg_method}/cvtest_Nlmb{Nlams}.txt', cvtest)
+np.savetxt(f'{args.data_func}/{args.reg_method}/cvtrain_Nlmb{Nlams}.txt', cvtrain)
+"""
--- Project1/header.py
+++ Project1/header.py
@@ -2,41 +2,106 @@
 
 # Import Library
 import sys
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import sklearn.linear_model as skl
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import  train_test_split
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer
-import scipy as scp
 import argparse
-from IPython.display import display
+import warnings
+import numpy as np
+import scipy as scp
+import pandas as pd
 from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-from mpl_toolkits.mplot3d import Axes3D
+from imageio import imread
 from scipy.stats import norm
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+warnings.filterwarnings('ignore')
+import sklearn.linear_model as skl
+from IPython.display import display
+from sklearn.linear_model import Ridge
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import  train_test_split
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer
+
+np.random.seed(2018) # Setting Random Seed constant
+
+# Set figure dimensions to avoid scaling in LaTeX.
+def set_size(width, fraction=1):
+    # Width of figure (in pts)
+    fig_width_pt = width * fraction
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+    # Golden ratio to set aesthetic figure height # https://disq.us/p/2940ij3
+    golden_ratio = (5**.5 - 1) / 2
+    # Manual addons
+    heightadd = inches_per_pt * 45
+    widthadd = inches_per_pt * 65
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt + widthadd
+    # Figure height in inches
+    fig_height_in = fig_width_in * golden_ratio + heightadd
+    fig_dim = (fig_width_in, fig_height_in)
+    return fig_dim
 
 # Vanilla 1-D Data Generation
-def simple_function(x,noise,noisy):
+def simple_function(x,noise):
     function_ = 7.2*x**5 + 0.5*x**2 # 2.5*np.e**-x + 0.72*x**x 
-    if noisy == True:
-        return function_+0.2*noise
-    else:
-        return function_
+    return function_+noise 
 
 # Franke Function
-def FrankeFunction(x,y,noise,noisy):
-    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-    if noisy == True:
-        return (term1+term2+term3+term4)+0.2*noise
-    else:
-        return term1 + term2 + term3 + term4
+def FrankeFunction(N,noise,print):
+    x = np.sort(np.random.uniform(0,1,N)) 
+    y = np.sort(np.random.uniform(0,1,N))
+    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2)) # First term
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1)) # Second term
+    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2)) # Third term
+    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2) # Fourth term
+    z = (term1+term2+term3+term4)+noise
+
+    # For printing plot
+    if print == True:
+        xmesh,ymesh = np.meshgrid(x,y)
+        fig = plt.figure(figsize=set_size(345), dpi=80)
+        ax = fig.gca(projection='3d')
+        surf = ax.plot_surface(xmesh, ymesh, z,\
+            cmap=cm.coolwarm,linewidth=0, antialiased=False) # Plot the surface.
+        ax.set_xlabel('x', linespacing=3.2)
+        ax.set_ylabel('y', linespacing=3.1)
+        ax.set_zlabel('z', linespacing=3.4)
+        fig.colorbar(surf, shrink=0.5)  # Add colorbar
+        # plt.savefig(f"results/FrankFunction_Noise:True.pdf", format='pdf', bbox_inches='tight')
+        plt.show()
+
+    return x,y,z # Final function plus eventual noise
+
+# Function for Terrain Data
+def RealFunction(N,noise,print):
+    terrain = imread('SRTM_data_Norway_1.tif')[::N,::N] # Load the terrain (scaled down)
+    xlen,ylen = np.shape(terrain) # Get the shape (not necessarily square image)
+    x = np.linspace(0,xlen-1,xlen)
+    y = np.linspace(0,ylen-1,ylen)
+    xmesh, ymesh = np.meshgrid(x, y, indexing='ij')
+    x_flat = xmesh.reshape(xmesh.shape[0] * xmesh.shape[1])  # flattens x
+    y_flat = ymesh.reshape(ymesh.shape[0] * ymesh.shape[1])  # flattens y
+    z_flat = terrain.reshape(terrain.shape[0]*terrain.shape[1]) # flattens z 
+    
+    # For printing plot
+    if print == True:
+        fig = plt.figure(figsize=set_size(345),dpi=80)
+        ax = fig.gca(projection='3d')
+        surf = ax.plot_surface(xmesh,ymesh,terrain,cmap=cm.coolwarm,linewidth=0,antialiased=False)
+        # ax.view_init(elev=90, azim=0)
+        fig.colorbar(surf,shrink=0.5)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        # plt.savefig(f'results/TerrainCMtopview.pdf', format='pdf', bbox_inches='tight')
+        # plt.savefig(f'results/TerrainCM.pdf', format='pdf', bbox_inches='tight')
+        plt.show()
+    
+    return x_flat,y_flat,z_flat # Returns flattened arrays of x,y, and z (the image)
 
 # Generating Design Matrix
 def create_X(x,y,n):
@@ -56,15 +121,48 @@ def create_X(x,y,n):
     #     X[:,i] = x**i
 
 '''
-****************************************************************************************
+---****************************---
+The following are functions for 
+finding optimal beta depending 
+on the regression method
 '''
 
-# Function for performing OLS
-def OLSlinreg(X,f):
+# Function for performing OLS regression
+def OLSlinreg(X,f,lmb):
     A = np.linalg.pinv(X.T.dot(X)) # SVD inverse
     beta = A.dot(X.T).dot(f)
     return beta # Returns optimal beta
-    
+
+# Function for performing Ridge regression given a lambda
+def Ridgelinreg(X,f,lmb):
+    I = np.eye(X.shape[1])
+    A = np.linalg.pinv(X.T @ X + lmb*I) # SVD inverse
+    beta = A @ X.T @ f
+    return beta # Returns optimal beta 
+
+# Functin for performing Lasso regression given a lambda 
+def Lassolinreg(X,f,lmb): 
+    RegLasso = linear_model.Lasso(lmb) # fit_intercept=false? to remove intercept
+    RegLasso.fit(X,f)
+    return RegLasso.coef_
+
+'''
+---****************************---
+'''
+
+# Mean Squared Error (MSE)
+def MSE_func(y_data,y_model):
+    n = np.size(y_model)
+    return np.sum((y_data-y_model)**2)/n 
+
+# R2 Score
+def R2(y_data, y_model):
+    return 1-np.sum( (y_data - y_model)**2) / np.sum( (y_data - np.mean(y_data)) ** 2 )
+
+# No Data Scale
+def noscale(xtrain,xtest,ytrain,ytest):
+    return xtrain,xtest,ytrain,ytest
+
 # Scaling Data 
 def scale_data(xtrain,xtest,ytrain,ytest):
     scaler = StandardScaler()
@@ -76,104 +174,14 @@ def scale_data(xtrain,xtest,ytrain,ytest):
     ytestscaled = scaler.transform(ytest.reshape(-1, 1)).ravel()
     return xtrainscaled, xtestscaled, ytrainscaled, ytestscaled
 
-# Mean Squared Error (MSE)
-def MSE_func(y_data,y_model):
-    n = np.size(y_model)
-    return np.sum((y_data-y_model)**2)/n 
-
-# R2 Score
-def R2(y_data, y_model):
-    return 1-np.sum( (y_data - y_model)**2) / np.sum( (y_data - np.mean(y_data)) ** 2 )
-
-# Relative error 
-def RelativeError_func(y_data,y_model):
-    return abs((y_data-y_model)/y_data)
-
-# For plotting beta as increasing order of polynomials 
-def plot_beta_save(betavals,beta):
-    return betavals.append(beta)
-
-# Boostrap Resampling method
-def bootstraping(xtrain,ytrain):
+# Boostrap resampling
+def bootstrap(xtrain,ytrain):
     N = len(xtrain)
     ind = np.random.randint(0,N,size=(N,))
     xprime = xtrain[ind]
     funcprime = ytrain[ind]
     return xprime,funcprime
 
-# MSE and R2 via OLS
-def OLS_learning(x,y,n,func,phi,noisy):
-    # Initiate Containers
-    degree_beta = []
-    r2test = np.zeros(n)
-    r2train = np.zeros(n)
-    MSE_test = np.zeros(n)
-    MSE_train = np.zeros(n)
-    R2_sklTest = np.zeros(n)
-    R2_sklTrain = np.zeros(n)
-    MSE_sklTest = np.zeros(n)
-    MSE_sklTrain = np.zeros(n)
-
-    # Loop from degree 1 to max_degree polynomials
-    for degree in phi: 
-        X = create_X(x,y,degree) # Build Design Matrix
-
-        # Splitting the Data
-        X_train, X_test, y_train, y_test = train_test_split\
-          (X,func, test_size = 0.2, random_state=1) 
-
-        # # Scale the Data
-        # X_train, X_test, y_train, y_test = scale_data(X_train,X_test,y_train,y_test)
-        
-        # Training 
-        beta = OLSlinreg(X_train,y_train) # Calculate Beta 
-        ytilde = X_train @ beta
-        # plot_beta_save(degree_beta,beta) # For plotting beta as increasing order of polynomials 
-
-        # Testing
-        ypredict = X_test @ beta
-
-        # MSE & R2 score via own Algorithm
-        MSE_train[degree-1] = MSE_func(y_train,ytilde)  
-        MSE_test[degree-1] =  MSE_func(y_test,ypredict) 
-        r2train[degree-1] = R2(y_train,ytilde)
-        r2test[degree-1] = R2(y_test,ypredict)
-
-        # SciKitLearnRegCheck
-        clf = skl.LinearRegression().fit(X_train,y_train) # fit_intercept=False ?
-        MSE_sklTrain[degree-1] = mean_squared_error(clf.predict(X_train),y_train)
-        MSE_sklTest[degree-1] = mean_squared_error(clf.predict(X_test),y_test)
-        R2_sklTrain[degree-1] = clf.score(X_train,y_train)
-        R2_sklTest[degree-1] = clf.score(X_test,y_test)
-
-    # For plotting beta as increasing order of polynomials    
-    # BetaValues = pd.DataFrame(degree_beta)
-    # BetaValues.to_csv(f'results/BetaValsDegNoise:{noisy}.csv', index=False)
-
-    return (MSE_train,MSE_test,MSE_sklTrain,MSE_sklTest,\
-        r2train,r2test,R2_sklTrain,R2_sklTrain)
-
-# OLS MSE bootstrap resampling 
-def OLS_boots(x,y,n,func,phi,nB):
-    var = np.zeros(n)
-    bias = np.zeros(n)
-    msesamp = np.zeros((n,nB))
-
-    for degree in phi:
-        X = create_X(x,y,degree) # Build Design Matrix
-        # Splitting the Data
-        X_train, X_test, y_train, y_test = train_test_split\
-            (X,func, test_size = 0.2, random_state=1)
-        ypred = np.empty((y_test.shape[0],nB))
-        for boots in range(0,nB):
-            # Sample Data
-            X_trboot, y_trboot = bootstraping(X_train,y_train)
-            beta = OLSlinreg(X_trboot,y_trboot) # Calculate Beta
-            ypred[:,boots] = ypr = (X_test @ beta).ravel()  # Testing
-            msesamp[degree-1,boots] = MSE_func(y_test,ypr)#ypred[:,boots]) # Calculate MSE
-        bias[degree-1] = np.nanmean( (y_test - np.nanmean(ypred, axis=1, keepdims=True))**2 )
-        var[degree-1] = np.mean( np.nanvar(ypred, axis=1) )
-    return msesamp,bias,var 
-
-    
-    
+# For plotting beta as increasing order of polynomials 
+def plot_beta_save(betavals,beta):
+    return betavals.append(beta)
