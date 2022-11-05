ae1f3a9c3459d43fe5a1e8711a1fadc04294e534
Nazzza <alimdnazer@gmail.com>
2022-11-05 12:23:00 +0100

    checkpoint1.1

--- Project2/OLS_GD_M_Fra.py
+++ Project2/OLS_GD_M_Fra.py
@@ -1,23 +1,8 @@
 """"
 Outline for naked OLS regression with Gradient Descent 
 -on the Franke Function without noise WITH momentum 
-1. Imports 
-2. Initialize Franke function
-3. Set size()
-4. runMSE()
-5. Seed & Initialize variables 
-6. Setting up Design matrix
-7. Hessian matrix
-8. OLS regression
-9. Fetch and print eigenvalues
-10. Learning rate
-11. Gradient Descent loop
-12. Design matrix specifically for ypredict
-13. Printing shape of ypredicts
-14. Running MSE on these funcs
-15. Printing MSE for GD, OLS, LR... ask Don abt this 
 """
-# 1. Importing various packages
+# Importing various packages
 from random import random, seed
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,7 +11,7 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import sys
 
-# 2. initializing starter variables 
+# initializing starter variables 
 #rando seed
 np.random.seed(2021) 
 #number of datapoints + No. of rows in design matrix // corr. to # of inputs to outputs
@@ -36,7 +21,7 @@ n = 5
 # No. of elements in beta // Number of columns in design matrix 
 l = int((n+1)*(n+2)/2)  
 
-# 3. Franke function
+# Franke function
 x = np.sort(np.random.uniform(0,1,N)) 
 y = np.sort(np.random.uniform(0,1,N))
 term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2)) # First term
@@ -46,7 +31,7 @@ term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2) # Fourth term
 z = (term1+term2+term3+term4)
 
 
-# 4. set size of plots - not needed yet 
+# set size of plots - not needed yet 
 def set_size(width, fraction=1):
     # Width of figure (in pts)
     fig_width_pt = width * fraction
@@ -65,7 +50,7 @@ def set_size(width, fraction=1):
     return fig_dim
 
 
-# 5. mse
+# mse
 def run_mse(y_data,y_model):
     n = np.size(y_model)
     return np.sum((y_data-y_model)**2)/n     
@@ -74,7 +59,7 @@ def run_mse(y_data,y_model):
 
 
 
-# 6. Frank Function 2D Design Matrix X
+# Frank Function 2D Design Matrix X
 X = np.ones((N,l))      
 for i in range(1,n+1):  # Loop through features 1 to n (skipped 0)
     q = int((i)*(i+1)/2)    
@@ -83,11 +68,11 @@ for i in range(1,n+1):  # Loop through features 1 to n (skipped 0)
 
 print("X shape", X.shape)
 
-# 7. Hessian matrix
+# Hessian matrix
 Hes_Mat = (2.0/n)* X.T @ X
 
 
-#8. OLS regressionn
+# OLS regressionn
 beta_linreg = np.linalg.inv(X.T @ X) @ X.T @ z
 
 #find out what this is !!!
@@ -95,19 +80,19 @@ beta = np.random.randn(21, )
 nu_beta = np.random.randn(21, )
 
 
-#9. Fetch and print eigenvalues
+# Fetch and print eigenvalues
 EigValues, EigVectors = np.linalg.eig(Hes_Mat)
 # print(f"Eigenvalues of Hessian Matrix:{EigValues}")
 
 
-# 10. Learning rate
+# Learning rate
 lear_rate = 0.002
 #gamma parameter - leraning rate
 gamma = 1.0/np.max(EigValues)
 N_iterations = 1000
 
 
-#11. gradient descent loop
+# gradient descent loop
 for iter in range(N_iterations):
     gradient = (2.0/n)*X.T @ (X @ beta-z)
     nu_gradient = (2.0/n)*X.T @ (X @ nu_beta-z)
@@ -119,27 +104,27 @@ for iter in range(N_iterations):
 
 
 
-# 12. Design matrix specifically for ypredict
+# Design matrix specifically for ypredict
 ypredict_1 = X.dot(beta)
 ypredict_2 = X.dot(beta_linreg)
 ypredict_3 = X.dot(nu_beta)
 
-# 13. Printing shape of ypredicts
+# Printing shape of ypredicts
 print("ypredict1 shape", ypredict_1.shape)
 print("ypredict2 shape", ypredict_2.shape)
 print("ypredict3 shape", ypredict_3.shape)
 
-# 14. Running MSE on these funcs
+# Running MSE on these funcs
 mse_grad = run_mse(z, ypredict_1)
 mse_OLS = run_mse(z, ypredict_2)
 mse_gam = run_mse(z, ypredict_3)
 
-# 15. Printing MSE
+# Printing MSE
 print("MSE Grad: ", mse_grad)
 print("MSE OLS: ", mse_OLS)
 print("MSE Gam: ", mse_gam)
 
-# 16. Plotting functions 
+# Plotting functions 
 # MSE vs complexity?
 # MSE vs epoch? - # of times a complete dataset is passed through the algorithm  
 # ^^ vs batch - small groups of a dataset is passed through the algorithm 
