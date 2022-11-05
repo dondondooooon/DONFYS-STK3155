e1d166aa03ca66bfe87a84c85705c812f6878632
Nazzza <alimdnazer@gmail.com>
2022-11-05 12:33:47 +0100

    gradient_loop working

--- Project2/OLS_GD_M_Fra.py
+++ Project2/OLS_GD_M_Fra.py
@@ -24,15 +24,23 @@ l = int((n+1)*(n+2)/2)
 # Franke function
 x = np.sort(np.random.uniform(0,1,N)) 
 y = np.sort(np.random.uniform(0,1,N))
+#z = np.sort(np.random.uniform(0,1,N))
 term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2)) # First term
 term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1)) # Second term
 term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2)) # Third term
 term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2) # Fourth term
 z = (term1+term2+term3+term4)
-#gotta figure out why a FUNCTION for franke function fucks up the MSE
+#gotta figure out why a FUNCTION for franke function fucks up the MSE'
+# def franke_function(z):
+#     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2)) # First term
+#     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1)) # Second term
+#     term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2)) # Third term
+#     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2) # Fourth term
+#     z = (term1+term2+term3+term4)
+#     return z
 
 
-# mse
+# mse function 
 def run_mse(y_data,y_model):
     n = np.size(y_model)
     return np.sum((y_data-y_model)**2)/n     
@@ -51,10 +59,11 @@ Hes_Mat = (2.0/n)* X.T @ X
 
 
 # OLS regressionn
-beta_linreg = np.linalg.inv(X.T @ X) @ X.T @ z
+beta_ols = np.linalg.inv(X.T @ X) @ X.T @ z
 
 #find out what this is !!!
-beta = np.random.randn(21, )
+beta_gradient = np.random.randn(21, )
+beta_gamma = np.random.randn(21, )
 
 # Fetch and print eigenvalues
 EigValues, EigVectors = np.linalg.eig(Hes_Mat)
@@ -65,23 +74,29 @@ EigValues, EigVectors = np.linalg.eig(Hes_Mat)
 lear_rate = 0.002
 #gamma parameter - leraning rate
 gamma = 1.0/np.max(EigValues)
-N_iterations = 1000
+n_iterations = 1000
 
 
 # gradient descent loop
-for iter in range(N_iterations):
-    gradient = (2.0/n)*X.T @ (X @ beta-z)
-    #add stopping criteria here
-    # if gradient < 10:
-    #updating beta
-    beta = beta - (gamma*gradient) #()should be -learning rate * gradient
+def gradient_loop():
+    for _ in range(n_iterations):
+        global beta_gradient
+        gradient = (2.0/n)*X.T @ (X @ beta_gradient-z)
+        #add stopping criteria here
+        # if gradient < 10:
+        #updating beta
+        beta_gradient = beta_gradient - (gamma*gradient) #()should be -learning rate * gradient
+        print("Gradient Beta #", _+1, beta_gradient)
+    return beta_gradient
 
+# calling gradient_loop
+gradient_loop()
 
 
 # Design matrix specifically for ypredict
-ypredict_1 = X.dot(beta)
-ypredict_2 = X.dot(beta_linreg)
-ypredict_3 = X.dot(nu_beta)
+ypredict_1 = X.dot(beta_gradient)
+ypredict_2 = X.dot(beta_ols)
+ypredict_3 = X.dot(beta_gamma)
 
 # Running MSE on these funcs
 mse_grad = run_mse(z, ypredict_1)
@@ -89,9 +104,9 @@ mse_OLS = run_mse(z, ypredict_2)
 mse_gam = run_mse(z, ypredict_3)
 
 # Printing MSE
-print("MSE Grad: ", mse_grad)
-print("MSE OLS: ", mse_OLS)
-print("MSE Gam: ", mse_gam)
+print("Gradient MSE: ", mse_grad)
+print("OLS MSE: ", mse_OLS)
+print("Gamma MSE: ", mse_gam)
 
 # Plotting functions 
 # MSE vs complexity?
