f904ffa6556ec014f96ddc2b776a57dd0a4b0bd5
Nazzza <alimdnazer@gmail.com>
2022-11-13 05:05:58 +0100

    reverted some changes and changed a bit more, commented for clarity

--- Project2/GD_M.py
+++ Project2/GD_M.py
@@ -8,67 +8,100 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import sys
 from sklearn.model_selection import train_test_split
 
-# the number of datapoints
-n = 100
-x = np.random.rand(n,1)
-y = 3.0*x**2 - 3.0*x + 1 + 0.1*np.random.randn(n,1)
+from numpy import asarray
+from numpy import arange
+from numpy.random import rand
+from numpy.random import seed
+from matplotlib import pyplot
+import autograd.numpy as np
+from autograd import grad
+ 
+# objective function
+def objective(x):
+	return 4.0*x**2.0 + 3.0*x + 1.0
+ 
+# derivative of objective function
+def derivative(x):
+	return x * 8.0 + 3.0
+#obj_grad = grad(objective)
 
-# 2nd order polynomial in func form 
-def start_f():
-    return 3.0*x**2 - 3.0*x + 1 + 0.1*np.random.randn(n,1)
-
-# derived 2nd order polynomial in func form 
-def der_f():
-    return 3.0*x**2 - 3.0*x + 1 + 0.1*np.random.randn(n,1)
-
-# mse func
-def run_mse(y_data,y_model):
-    n = np.size(y_model)
-    return np.sum((y_data-y_model)**2)/n  
-#R2 func
-def run_r2(y_data, y_model):
-    return 1 - np.sum((y_data - y_model) ** 2) / np.sum((y_data - np.mean(y_data)) ** 2)
-
-# Design matrix 
-X = np.c_[np.ones((n,1)), x]
-XT_X = X.T @ X
-# Hessian matrix
-H = (2.0/n)* X.T @ X
-# Get the eigenvalues
-EigValues, EigVectors = np.linalg.eig(H)
-#print(f"Eigenvalues of Hessian Matrix:{EigValues}")
-
-# train test split 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# OLS regression 
-beta_linreg = np.linalg.inv(XT_X) @ (X.T @ y)
-#printing  beta of regression
-#print(beta_linreg)
-beta = np.random.randn(2,1)
-
-# ETA IS INVERSE OF HES MATRIX? POSSIBLY 
-eta = 1.0/np.max(EigValues)
-N_iterations = 1000
-
-# Gradient descent loop
-for iter in range(N_iterations):
-    gradient = (2.0/n)*X.T @ (X @ beta-y)
-    beta -= eta*gradient
-    # print to see betas per iter, remember to reduce iterations 
-    #print(iter, gradient[0], gradient[1])
-    
-# Printing beta after gd loop
-# print(beta)
-
-# Predicting mse, r2 and relative error for training data
-ytilde = X_train @ beta
-ypredict = X_test @ beta
-
-print("\n--Training data eval--")
-print("R2: ", run_r2(y_train,ytilde))
-print("MSE: ", run_mse(y_train,ytilde))
-
-print("--Test data eval--")
-print("R2: ", run_r2(y_test,ypredict))
-print("MSE: ", run_mse(y_test,ypredict), "\n")
+# gradient descent algorithm - calculates gradients per step, momentum saves the data
+def gd2(objective, derivative, bounds, n_iter, step_size, momentum):
+	solutions, scores = list(), list()
+	solution = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
+	change = 0.0
+	print("\n")
+	# run the gradient descent
+	for i in range(n_iter):
+		# calculate gradient <- not working since we're looking at the directory of solutions
+		# and not actually a point x 
+		# obj_grad = grad(objective)    
+		# gradient = obj_grad(solution)
+		gradient = derivative(solution)
+		# calculate update
+		new_change = step_size * gradient + momentum * change
+		# take a step
+		solution = solution - new_change
+		# save the change
+		change = new_change
+		# evaluate candidate point
+		solution_eval = objective(solution)
+		# store solution
+		solutions.append(solution)
+		scores.append(solution_eval)
+		# report progress
+		print('Loop #%d grad(%s) = %.5f' % (i+1, solution, solution_eval))
+	print("\n")
+	return [solutions, scores]
+ 
+# gradient descent algorithm - calculates gradients per step, momentum saves the data
+def gd(objective, derivative, bounds, n_iter, step_size, momentum):
+	# track all solutions
+	solutions, scores = list(), list()
+	# generate an initial point
+	solution = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
+	# keep track of the change
+	change = 0.0
+	# run the gradient descent
+	print(type(solution))
+	for i in range(n_iter):
+		print("gradient at ", solution)
+		# calculate gradient   
+		gradient = derivative(solution)
+		# calculate update
+		new_change = step_size * gradient + momentum * change
+		# take a step
+		solution = solution - new_change
+		# save the change
+		change = new_change
+		# evaluate candidate point
+		solution_eval = objective(solution)
+		# store solution
+		solutions.append(solution)
+		scores.append(solution_eval)
+		# report progress
+		print('>%d f(%s) = %.5f' % (i, solution, solution_eval))
+	return [solutions, scores]
+ 
+# seed the pseudo random number generator
+seed(4)
+# define range for input
+bounds = asarray([[-1.0, 1.0]])
+# define the total iterations
+n_iter = 5
+# define the step size
+step_size = 0.1
+# define momentum
+momentum = 0.3
+# perform the gradient descent search with momentum
+solutions, scores = gd2(objective, derivative, bounds, n_iter, step_size, momentum)
+# sample input range uniformly at 0.1 increments
+inputs = arange(bounds[0,0], bounds[0,1]+0.1, 0.1)
+# compute targets
+results = objective(inputs)
+# create a line plot of input vs result
+pyplot.plot(inputs, results)
+# plot the solutions found
+pyplot.plot(solutions, scores, '.-', color='red')
+# show the plot
+# pyplot.show()
