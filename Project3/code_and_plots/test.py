a41c35faca99bbaf1cb273e20a1f8d22ab7ebe76
dondondooooon <89593997+dondondooooon@users.noreply.github.com>
2022-11-24 14:59:45 +0100

    added project3 dir

---
+++ Project3/code_and_plots/test.py
@@ -0,0 +1,13 @@
+# import sys
+# sys.path.insert(0,"../Project1/code_and_plots/")
+# from header import set_size, FrankeFunction, create_X, OLSlinreg, Ridgelinreg, MSE_func, R2
+from nn import *
+import seaborn as sns
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from matplotlib import ticker as mtick
+from sklearn.model_selection import  train_test_split
+
+# np.random.seed(2022)
+# sns.set()
+
